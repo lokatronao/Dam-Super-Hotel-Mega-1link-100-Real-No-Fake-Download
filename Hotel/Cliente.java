22838f9ed73af685f76dc80b8d9e7443a8d4c404
lokatronao <lokatronao@gmail.com>
2018-02-13 11:06:19 +0100

    Creación de objeto cliente que es herencia de personas

---
+++ Hotel/Cliente.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package Hotel;
+
+/**
+ * @author lokatronao
+ *
+ */
+public class Cliente extends Persona {
+
+	/**
+	 * 
+	 */
+	protected Cliente() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param nombre
+	 * @param apellidos
+	 * @param dni
+	 * @param direccion
+	 */
+	protected Cliente(String nombre, String apellidos, String dni, String direccion) {
+		super(nombre, apellidos, dni, direccion);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param nombre
+	 * @param apellidos
+	 * @param dni
+	 */
+	protected Cliente(String nombre, String apellidos, String dni) {
+		super(nombre, apellidos, dni);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
